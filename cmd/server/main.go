fdeb8f6c02729b4f64b7f099ca77bfb3f17f5c05
Kira1108 <327485253@163.com>
2021-06-11 13:04:59 +0800

    Initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up Our App")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest Api Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting rest api")
+		fmt.Println(err)
+	}
 }
